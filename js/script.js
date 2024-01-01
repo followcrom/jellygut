fe591e80da63ee874c1bea207c05d4168adbf8ab
FollowCrom <followcrom@gmail.com>
2024-01-01 14:36:53 +0000

    Update service worker path and add site.webmanifest

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/service-worker.js").then(
+    navigator.serviceWorker.register("/js/service-worker.js").then(
       (registration) => {
         console.log(
           "ServiceWorker registration successful with scope: ",
